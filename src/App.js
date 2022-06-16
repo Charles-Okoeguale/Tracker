07aff0c2c5d435cb8cd615042ca65a73d5dc4b19
Charles <egualecharles@gmail.com>
2022-06-16 15:50:24 +0100

    chore: integrate with CoinGecko api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+      )
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
   return (
     <div className="coin-app">
       <div className='coin-search'>
@@ -10,6 +30,7 @@ function App() {
               type="text"
               className='coin-input'
               placeholder='search'
+              onChange={handleChange}
                />
           </form>
       </div>
